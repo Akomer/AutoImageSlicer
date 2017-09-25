babb6b032d291f7a72a2dd756a5097880fb90cc3
Akomer <spdrhu@gmail.com>
2017-09-25 20:39:47 +0200

    Add load image and save it

--- ImageSlicer/ViewModel/MainWindowViewModel.cs
+++ ImageSlicer/ViewModel/MainWindowViewModel.cs
@@ -1,13 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
+using ImageSlicer.Model;
 
 namespace ImageSlicer.ViewModel
 {
     class MainWindowViewModel : ViewModelBase
     {
+        private List<MultiImage> images;
 
+        public DelegateCommand LoadImageCommand { get; private set;}
+
+        public MainWindowViewModel()
+        {
+            LoadImageCommand = new DelegateCommand(x => LoadImage());
+            images = new List<MultiImage>();
+        }
+
+        private void LoadImage()
+        {
+            using (var dialoge = SinglePictureOpenDialoge())
+            {
+                if (dialoge.ShowDialog() == DialogResult.OK)
+                {
+                    images.Add(new MultiImage(dialoge.FileName));
+                }
+            }
+        }
+
+        private static OpenFileDialog SinglePictureOpenDialoge()
+        {
+            return new OpenFileDialog()
+            {
+                Filter = "Images (.png, .jpg, .bmp)|*.jpg;*.png;*.bmp|All Files (*.*)|*.*",
+                FilterIndex = 1,
+                Multiselect = false
+            };
+        }
     }
 }
